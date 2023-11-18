f985bcb37b7e14fbdb1fc85faa96900cf66c3089
Shafin Ahmed <kazishafin@gmail.com>
2023-11-18 11:26:02 -0500

    Update player stats page design

--- app/(pages)/player/[player-tag]/page.tsx
+++ app/(pages)/player/[player-tag]/page.tsx
@@ -1,18 +1,36 @@
-import { getPlayerAccount, getPlayerMatches } from "../../../../apis/api";
-import { getKASTForMatch, getPlayerAvgStatsForMatch } from "../../../helpers";
+import {
+  getPlayerAccount,
+  getPlayerMMR,
+  getPlayerMatches,
+} from "../../../../apis/api";
+import { Card } from "@tremor/react";
+import { PlayerAccuracyCard } from "../../../components/playerAccuracyCard";
 import { PlayerCardImage } from "../../../components/playerCardImage";
-import { PlayerTagLine } from "../../../components/playerTagline";
 import { PlayerGameName } from "../../../components/playerGameName";
+import { PlayerOverallAverageStatsCard } from "../../../components/playerOverallStatsCard";
+import { PlayerTagLine } from "../../../components/playerTagline";
+import {
+  getAvgStatsArrayForMatches,
+  getOverallAverageStats,
+} from "../../../helpers";
+import styles from "./page.module.css";
+import { StatAreaChartCard } from "../../../components/statAreaChartCard";
 
 const Page = async () => {
-  const playerAccount = await getPlayerAccount("SEN tarik", "1337");
+  const playerAccount = await getPlayerAccount("PlzHireMeAsDev", "layof");
 
   const recentMatches = await getPlayerMatches(
     playerAccount.region,
-    "SEN tarik",
-    "1337",
-    1,
+    playerAccount.name,
+    playerAccount.tag,
+    10,
     "competitive"
+  );
+
+  const playerMMR = await getPlayerMMR(
+    playerAccount.region,
+    playerAccount.name,
+    playerAccount.tag
   );
 
   const playedAgents = {} as Record<string, number>;
@@ -28,39 +46,80 @@ const Page = async () => {
     }
   });
 
-  const KAST = getKASTForMatch(playerAccount.puuid, recentMatches[0].rounds);
-
-  const player = recentMatches[0].players.all_players.find(
-    (matchPlayer) => matchPlayer.puuid === playerAccount.puuid
+  const avgStatsArray = getAvgStatsArrayForMatches(
+    recentMatches,
+    playerAccount.puuid
   );
 
-  if (!player) throw new Error("Could not find player in match!");
-
-  const playerAvgStats = getPlayerAvgStatsForMatch(
-    player,
-    recentMatches[0].rounds.length
+  const overallAverageStats = getOverallAverageStats(
+    avgStatsArray,
+    recentMatches
   );
 
   console.log(`
-  KAST      = ${KAST}
-  ADD       = ${playerAvgStats.AvgDamageDeltaForMatch}
-  ACS       = ${playerAvgStats.ACSForMatch}
-  ADR       = ${playerAvgStats.ADRForMatch}
-  Headshot% = ${playerAvgStats.avgHeadShotPercentage}
-  Bodyshot% = ${playerAvgStats.avgBodyShotPercentage}
-  Legshot%  = ${playerAvgStats.avgLegShotPercentage}
-
+  Stats across ${recentMatches.length} matches
+  KAST      = ${overallAverageStats.KAST}
+  ADD       = ${overallAverageStats.AvgDamageDeltaForMatch}
+  ACS       = ${overallAverageStats.ACSForMatch}
+  ADR       = ${overallAverageStats.ADRForMatch}
+  Headshot% = ${overallAverageStats.avgHeadShotPercentage}
+  Bodyshot% = ${overallAverageStats.avgBodyShotPercentage}
+  Legshot%  = ${overallAverageStats.avgLegShotPercentage}
+  KD        = ${overallAverageStats.KD}
   `);
+
   return (
-    <>
-      <div>
+    <div className={styles.page_container}>
+      <div className={styles.image_name_container}>
         <PlayerCardImage src={playerAccount.card.small} />
         <div>
-          <PlayerGameName gameName={playerAccount.name} />{" "}
-          <PlayerTagLine tagLine={playerAccount.tag} />
+          <PlayerGameName
+            gameName={playerAccount.name}
+            fontSize="2rem"
+            fontWeight="500"
+          />{" "}
+          <PlayerTagLine tagLine={playerAccount.tag} fontSize="1.825rem" />
         </div>
       </div>
-    </>
+      <div className={styles.stats_container}>
+        <div className={styles.sidebar_container}>
+          <Card>
+            Game Mode
+            <select>
+              <option>123</option>
+            </select>
+          </Card>
+          <Card>
+            Current Rating
+            <img src={playerMMR.current_data.images.small} />
+            {playerMMR.current_data.currenttierpatched}
+            {/* Peak Rating */}
+          </Card>
+          <PlayerOverallAverageStatsCard
+            overallAverageStats={overallAverageStats}
+          />
+          <PlayerAccuracyCard
+            numGames={recentMatches.length}
+            headPercent={Number(
+              overallAverageStats.avgHeadShotPercentage.toFixed(1)
+            )}
+            bodyPercent={Number(
+              overallAverageStats.avgBodyShotPercentage.toFixed(1)
+            )}
+            legsPercent={Number(
+              overallAverageStats.avgLegShotPercentage.toFixed(1)
+            )}
+          />
+          <StatAreaChartCard data={avgStatsArray} />
+          <Card>Maps??</Card>
+        </div>
+
+        <div className={styles.matches_container}>
+          <Card>Agents DEETS</Card>
+          <Card>Match Histyory</Card>
+        </div>
+      </div>
+    </div>
   );
 };
 
