ab038dfb1fbd6abbe59b93b243191ff8e17e1837
Shafin Ahmed <kazishafin@gmail.com>
2023-11-18 12:11:18 -0500

    Remove old card component

--- app/components/card.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./card.module.css";
-
-interface cardProps {
-  children: React.ReactNode;
-}
-
-export const Card = ({ children }: cardProps) => (
-  <div className={styles.card_container}>{children}</div>
-);
