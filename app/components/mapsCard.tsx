92f839f75f3a52566ce8550d2ec5b26e4685e4ed
Shafin Ahmed <kazishafin@gmail.com>
2023-11-19 14:02:39 -0500

    Refactor mapsCard to inner component

--- app/components/mapsCard.tsx
+++ app/components/mapsCard.tsx
@@ -6,6 +6,37 @@ interface mapsCardProps {
   mapsData: MapData;
 }
 
+interface mapDetailProps {
+  mapName: string;
+  stats: {
+    win: number;
+    loss: number;
+    imgSrc?: string;
+  };
+}
+
+const MapDetail = ({ mapName, stats }: mapDetailProps) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${stats.imgSrc})`,
+      }}
+      className={styles.map_container}
+    >
+      <div className={styles.map_inner_container}>
+        <div className={styles.map_name}>{mapName}</div>
+        <div className={styles.stats_container}>
+          <span>{stats.win}</span>
+          <span className={styles.win}>{` W`}</span>
+          {" - "}
+          <span>{stats.loss}</span>
+          <span className={styles.loss}>{` L`}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const MapsCard = ({ mapsData }: mapsCardProps) => {
   const numGames = Object.values(mapsData).reduce((acc, map) => {
     acc += map.win + map.loss;
@@ -16,28 +47,9 @@ export const MapsCard = ({ mapsData }: mapsCardProps) => {
     <Card>
       <span className={styles.map_title}>Maps - Last {numGames} games</span>
       <div className={styles.maps_container}>
-        {Object.entries(mapsData).map(([mapName, stats]) => {
-          return (
-            <div
-              key={mapName}
-              style={{
-                backgroundImage: `url(${stats.imgSrc})`,
-              }}
-              className={styles.map_container}
-            >
-              <div className={styles.map_inner_container}>
-                <div className={styles.map_name}>{mapName}</div>
-                <div className={styles.stats_container}>
-                  <span>{stats.win}</span>
-                  <span className={styles.win}>{` W`}</span>
-                  {" - "}
-                  <span>{stats.loss}</span>
-                  <span className={styles.loss}>{` L`}</span>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+        {Object.entries(mapsData).map(([mapName, stats]) => (
+          <MapDetail key={mapName} mapName={mapName} stats={stats} />
+        ))}
       </div>
     </Card>
   );
