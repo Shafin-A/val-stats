cba4247fc74cabd07ed3773c43ba36c152679c5e
Shafin Ahmed <kazishafin@gmail.com>
2023-11-20 18:32:29 -0500

    Styled recent matches card component

--- app/components/cards/recentMatchesCard.tsx
+++ app/components/cards/recentMatchesCard.tsx
@@ -1,6 +1,7 @@
-import { Card } from "@tremor/react";
+import { Badge, Card } from "@tremor/react";
 import { formatDate, getAgentImg } from "../../helpers";
 import { Agent, Match } from "../../../types/types";
+import styles from "./recentMatchesCard.module.css";
 
 interface recentMatchesProps {
   recentMatches: Match[];
@@ -8,6 +9,28 @@ interface recentMatchesProps {
   agents: Agent[];
   matchesStats: Record<string, number>[];
 }
+
+interface matchPositionBadge {
+  position: number;
+}
+
+const MatchPositionBadge = ({ position }: matchPositionBadge) => {
+  const getPositionLabelAndColor = () => {
+    if (position === 1) return { label: "MVP", color: "red" };
+    if (position === 2) return { label: "2nd", color: "orange" };
+    if (position === 3) return { label: "3rd", color: "amber" };
+    return { label: `${position}th`, color: "gray" };
+  };
+
+  const { label, color } = getPositionLabelAndColor();
+
+  return (
+    <Badge className={styles.badge} color={color}>
+      {label}
+    </Badge>
+  );
+};
+
 export const RecentMatchesCard = ({
   recentMatches,
   playerPuuid,
@@ -51,28 +74,91 @@ export const RecentMatchesCard = ({
 
         const matchStartTimeUnix = match.metadata.game_start;
         const matchStartTimeFormatted = formatDate(matchStartTimeUnix);
+
         return (
           <div
             key={match.metadata.matchid}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
+            className={`${styles.match_container} ${styles.border_bottom} ${
+              isPlayerOnWinningTeam ? styles.win_gradient : styles.loss_gradient
+            }`}
           >
-            <div>{matchStartTimeFormatted}</div>
-            <div>{matchMap}</div>
-            <img src={agentImg} height="48px" width="48px" />
-            <div>{matchScore}</div>
-            <div>
-              {kills}/{deaths}/{assists}
+            <img
+              className={styles.agent_img}
+              src={agentImg}
+              alt={`player match agent - ${agentPlayed}`}
+            />
+            <div className={styles.match_time_map}>
+              <span className={styles.small_gray_text}>
+                {matchStartTimeFormatted}
+              </span>
+              <span className={styles.match_map_text}>{matchMap}</span>
             </div>
-            <div>{playerMatchPosition}</div>
-            <div>{Math.round(matchesStats[index].AvgDamageDeltaForMatch)}</div>
-            <div>{matchesStats[index].avgHeadShotPercentage.toFixed(1)}</div>
-            <div>{matchesStats[index].KAST.toFixed(1)}</div>
-            <div>{matchesStats[index].ACSForMatch.toFixed(1)}</div>
-            <div>{matchesStats[index].ADRForMatch.toFixed(1)}</div>
-            <div>{matchesStats[index].KD.toFixed(2)}</div>
+
+            <div className={styles.match_score}>
+              <div className={styles.match_score_text_container}>
+                <span>
+                  {playerTeam === "Blue"
+                    ? match.teams.blue.rounds_won
+                    : match.teams.red.rounds_won}
+                </span>
+                <span>:</span>
+                <span>
+                  {playerTeam === "Blue"
+                    ? match.teams.red.rounds_won
+                    : match.teams.blue.rounds_won}
+                </span>
+              </div>
+              <MatchPositionBadge position={playerMatchPosition} />
+            </div>
+
+            <div className={styles.stat_container}>
+              <span className={styles.small_gray_text}>K / D / A</span>
+              <span className={styles.stat_text}>
+                {kills} / {deaths} / {assists}
+              </span>
+            </div>
+
+            <div className={styles.stat_container}>
+              <span className={styles.small_gray_text}>K/D</span>
+              <span className={styles.stat_text}>
+                {matchesStats[index].KD.toFixed(2)}
+              </span>
+            </div>
+
+            <div className={styles.stat_container}>
+              <span className={styles.small_gray_text}>DDΔ</span>
+              <span className={styles.stat_text}>
+                {Math.round(matchesStats[index].AvgDamageDeltaForMatch)}
+              </span>
+            </div>
+
+            <div className={styles.stat_container}>
+              <span className={styles.small_gray_text}>HS%</span>
+              <span className={styles.stat_text}>
+                {matchesStats[index].avgHeadShotPercentage.toFixed(1)}
+              </span>
+            </div>
+
+            <div className={styles.stat_container}>
+              <span className={styles.small_gray_text}>ADR</span>
+              <span className={styles.stat_text}>
+                {matchesStats[index].ADRForMatch.toFixed(1)}
+              </span>
+            </div>
+
+            <div className={styles.stat_container}>
+              <span className={styles.small_gray_text}>ACS</span>
+              <span className={styles.stat_text}>
+                {matchesStats[index].ACSForMatch.toFixed(1)}
+              </span>
+            </div>
+
+            <div className={styles.stat_container}>
+              <span className={styles.small_gray_text}>KAST</span>
+              <span className={styles.stat_text}>
+                {matchesStats[index].KAST.toFixed(1)}
+              </span>
+            </div>
           </div>
         );
       })}
