39932dfb157c96cf2a243a523efe6f02163adf70
Shafin Ahmed <kazishafin@gmail.com>
2024-04-03 16:39:09 -0400

    Added match details to top of match card

--- app/components/cards/matchCard.tsx
+++ app/components/cards/matchCard.tsx
@@ -10,14 +10,49 @@ import {
 import styles from "./matchCard.module.css";
 import { Match } from "../../../types/types";
 import MatchTable from "../matchTable";
+import { formatDate } from "../../helpers";
 
 interface MatchCardProps {
   match: Match;
 }
 
 const MatchCard = ({ match }: MatchCardProps) => {
+  const gameLengthInSeconds = match.metadata.game_length;
+  const gameLengthDate = new Date(gameLengthInSeconds * 1000);
+
   return (
     <Card>
+      <div className={styles.match_data_container}>
+        <div className={styles.match_mode_map_container}>
+          <span className={styles.grey_text}>{match.metadata.mode}</span>
+          <span className={styles.bold_text}>{match.metadata.map}</span>
+        </div>
+        <div className={styles.match_score_container}>
+          <span className={styles.grey_text}>Score</span>
+          <div className={styles.match_score}>
+            <span className={styles.bold_text} style={{ color: "#74d5d3" }}>
+              {match.teams.blue.rounds_won}
+            </span>
+            <span className={styles.bold_text}>:</span>
+            <span className={styles.bold_text} style={{ color: "#df3a3a" }}>
+              {match.teams.red.rounds_won}
+            </span>
+          </div>
+        </div>
+        <div className={styles.match_length_container}>
+          <span className={styles.grey_text}>
+            {formatDate(match.metadata.game_start)}{" "}
+          </span>
+          <div>
+            <span className={styles.bold_text}>
+              {gameLengthDate.getMinutes()}m
+            </span>{" "}
+            <span className={styles.bold_text}>
+              {gameLengthDate.getSeconds()}s
+            </span>
+          </div>
+        </div>
+      </div>
       <TabGroup>
         <TabList className={styles.tab_list} variant="line" defaultValue="1">
           <Tab className={styles.tab} value="1">
